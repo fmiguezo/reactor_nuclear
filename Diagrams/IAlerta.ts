93456c116737486a5c211f3aee626496d263d4d7
M12 <matias.serro@gmail.com>
2024-06-08 15:27:53 -0300

    Elimino dos clases que no iban

--- Diagrams/IAlerta.ts
+++
@@ -1,8 +0,0 @@
-export default interface IAlerta {
-
-    obtenerTipoDeAlerta(c: ComprobadorSaludReactor): TipoAlerta;
-    obtenerMensajeDeAlerta(c: ComprobadorSaludReactor): string;
-    obtenerTimestampDeAlerta(c: ComprobadorSaludReactor): Date;
-    crearAlerta(): IAlerta;
-
-}
--- Diagrams/TipoAlerta.ts
+++
@@ -1,4 +0,0 @@
-const TipoAlerta = {
-    CRITICA: 'CRITICA',
-    ESTANDAR: 'ESTANDAR'
-};
