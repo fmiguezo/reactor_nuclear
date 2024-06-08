f33a4b2c9251389f432ffede83d3cc054801ca24
M12 <matias.serro@gmail.com>
2024-06-08 16:22:02 -0300

    Creo la clase notificadorDeAlerta

--- NotificadorDeAlerta.ts
+++ NotificadorDeAlerta.ts
@@ -3,7 +3,7 @@ import IAlerta from "./IAlerta";
 export default class NotificadorDeAlerta {
 
     notificarAlerta(alerta: IAlerta) {
-        
+        console.log(`Mensaje: ${alerta.obtenerMensajeDeAlerta} \nTipo de alerta: ${alerta.obtenerTipoDeAlerta} \nFecha: ${alerta.obtenerTimestampDeAlerta}`);
     }
 
 }
