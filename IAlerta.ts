1b7147754e22bc14aa177855030f546bb928138a
M12 <matias.serro@gmail.com>
2024-06-08 15:26:01 -0300

    Creo la interface IAlerta con sus metodos

---
+++ IAlerta.ts
@@ -0,0 +1,10 @@
+import TipoAlerta from "./TipoAlerta"
+
+export default interface IAlerta {
+
+    obtenerTipoDeAlerta(c: ComprobadorSaludReactor): TipoAlerta;
+    obtenerMensajeDeAlerta(c: ComprobadorSaludReactor): string;
+    obtenerTimestampDeAlerta(c: ComprobadorSaludReactor): Date;
+    crearAlerta(): IAlerta;
+
+}
