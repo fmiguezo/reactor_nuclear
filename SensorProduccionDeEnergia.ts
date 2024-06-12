5e89da9f81aaa9a7758295e10168164eb440b596
juan <jpverdondoni@gmail.com>
2024-06-12 14:37:40 -0300

    implemente los geters y setters de los atributos _activo y _energiaProducida

--- SensorProduccionDeEnergia.ts
+++ SensorProduccionDeEnergia.ts
@@ -1,8 +1,23 @@
 import ISensor from "./ISensor";
 
 export default class SensorProduccionDeEnergia implements ISensor{
+   
     private _activo: boolean = true;
     private _energiaProducida: number;
+
+    public get activo(): boolean {
+        return this._activo;
+    }
+    public set activo(value: boolean) {
+        this._activo = value;
+    }
+
+    public get energiaProducida(): number {
+        return this._energiaProducida;
+    }
+    public set energiaProducida(value: number) {
+        this._energiaProducida = value;
+    }
     estaActivo(): boolean {
        return this._activo;
     }
@@ -26,5 +41,7 @@ export default class SensorProduccionDeEnergia implements ISensor{
     public obtenerValor(): number {
         return this._energiaProducida;
     }
+
+    
 
 }
