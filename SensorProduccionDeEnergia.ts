1ebd6664f1c7bb60f0f57f871a102b2aebe5a5f7
juan <jpverdondoni@gmail.com>
2024-06-12 14:31:34 -0300

    implemente el activar en SensorProduccionDeEnergia

--- SensorProduccionDeEnergia.ts
+++ SensorProduccionDeEnergia.ts
@@ -6,9 +6,11 @@ export default class SensorProduccionDeEnergia implements ISensor{
     estaActivo(): boolean {
        return this._activo;
     }
-    activar(): void {
-        throw new Error("Method not implemented.");
+    
+    public activar(): void {
+        this._activo = true;
     }
+
     desactivar(): void {
         throw new Error("Method not implemented.");
     }
