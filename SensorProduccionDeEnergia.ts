f929cc5019727fc1ce7bbf336d24ca393bf48299
juan <jpverdondoni@gmail.com>
2024-06-12 15:01:39 -0300

    Inicialice _energiaProducida en 0, y cree un test de sensorTemperatura

--- SensorProduccionDeEnergia.ts
+++ SensorProduccionDeEnergia.ts
@@ -3,7 +3,7 @@ import ISensor from "./ISensor";
 export default class SensorProduccionDeEnergia implements ISensor{
    
     private _activo: boolean = true;
-    private _energiaProducida: number;
+    private _energiaProducida: number = 0;
 
     public get activo(): boolean {
         return this._activo;
