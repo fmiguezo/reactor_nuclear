250cbfe4ad0988807fb1182b792a11c1de503a60
juan <jpverdondoni@gmail.com>
2024-06-12 16:49:00 -0300

    Lo que hice fue hacer los test de SensorProduccionDe energia y el imine un getter que hace lo mismo que obtenerValor

--- SensorProduccionDeEnergia.ts
+++ SensorProduccionDeEnergia.ts
@@ -11,10 +11,7 @@ export default class SensorProduccionDeEnergia implements ISensor{
     public set activo(value: boolean) {
         this._activo = value;
     }
-
-    public get energiaProducida(): number {
-        return this._energiaProducida;
-    }
+   
     public set energiaProducida(value: number) {
         this._energiaProducida = value;
     }
@@ -31,10 +28,10 @@ export default class SensorProduccionDeEnergia implements ISensor{
     }
 
     public actualizarValor(energiaPRoducida:number): void {
-        if (this._activo) {
-            this._energiaProducida = energiaPRoducida;
-        } else {
+        if (!this._activo) {
             throw new Error("El sensor no está activo");
+        } else {
+            this._energiaProducida = energiaPRoducida;
         }
     }
 
