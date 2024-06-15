baba23459bfa43c0e63819658efa95087b225156
M12 <matias.serro@gmail.com>
2024-06-15 20:38:42 -0300

    Modifico la clase para convertirla en interface y así implementar el patrón Factory para la creación de los distintos tipos de alertas

--- GeneradorDeAlertas.ts
+++ GeneradorDeAlertas.ts
@@ -1,14 +1,5 @@
-import AlertaCritica from "./AlertaCritica";
-import AlertaEstandar from "./AlertaEstandar";
 import IAlerta from "./IAlerta";
 
-export default class GeneradorDeAlertas {
-    public generarAlerta(c: ComprobadorSaludReactor): IAlerta {
-        if(c.tieneTemperaturaSegura(r)) {
-            return new AlertaEstandar();
-        }
-        else {
-            return new AlertaCritica();
-        }
-    }
+export default interface GeneradorDeAlertas {
+    generarAlerta(c: ComprobadorSaludReactor): IAlerta;
 }
