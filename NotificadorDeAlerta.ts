d94b1dd4775a3745bf0226961c3e2067e9ac2a08
M12 <matias.serro@gmail.com>
2024-06-17 10:59:34 -0300

    Modifico el tipo de dato que retorna la func notificar alerta (ahora es un string), y creo los tests de la clase NotificadorDeAlerta

--- NotificadorDeAlerta.ts
+++ NotificadorDeAlerta.ts
@@ -2,8 +2,10 @@ import IAlerta from "./IAlerta";
 
 export default class NotificadorDeAlerta {
 
-    public notificarAlerta(alerta: IAlerta) {
-        console.log(`Mensaje: ${alerta.obtenerMensajeDeAlerta()} \nTipo de alerta: ${alerta.obtenerTipoDeAlerta()} \nFecha: ${alerta.obtenerTimestampDeAlerta()}`);
+    public notificarAlerta(alerta: IAlerta): string {
+        return `Mensaje: ${alerta.obtenerMensajeDeAlerta()} 
+        \nTipo de alerta: ${alerta.obtenerTipoDeAlerta()} 
+        \nFecha: ${alerta.obtenerTimestampDeAlerta()}`;
     }
 
 }
