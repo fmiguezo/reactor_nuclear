3ca62f36ddd072e5de0df5461d0dd1383e550dc8
matiGGV <matiggv@protonmail.com>
2024-06-19 01:01:37 -0300

    Imports corregidos en los archivos de Tests

--- Tests/SistemaDeControl/Alertas/NotificadorDeAlerta.test.ts
+++ Tests/SistemaDeControl/Alertas/NotificadorDeAlerta.test.ts
@@ -1,26 +1,25 @@
-import AlertaEstandar from "../AlertaEstandar";
-import NotificadorDeAlerta from "../NotificadorDeAlerta";
+import AlertaEstandar from "../../../SistemaDeControl/Alertas/AlertaEstandar";
+import NotificadorDeAlerta from "../../../SistemaDeControl/Alertas/NotificadorDeAlerta";
 
 describe("Test de la clase NotificadorDeAlerta", () => {
+  let instance: NotificadorDeAlerta;
+  let alertaEstandar: AlertaEstandar;
 
-    let instance: NotificadorDeAlerta;
-    let alertaEstandar: AlertaEstandar;
+  beforeEach(() => {
+    instance = new NotificadorDeAlerta();
+    alertaEstandar = new AlertaEstandar();
+    alertaEstandar.date = new Date("2024-06-08T12:00:00");
+    alertaEstandar.tipoAlerta = TipoAlerta.ESTANDAR;
+  });
 
-    beforeEach(() => {
-        instance = new NotificadorDeAlerta();
-        alertaEstandar = new AlertaEstandar();
-        alertaEstandar.date = new Date("2024-06-08T12:00:00");
-        alertaEstandar.tipoAlerta = TipoAlerta.ESTANDAR;
-    })
+  it("Verifica que la instancia sea de NotificadorDeAlerta", () => {
+    expect(instance).toBeInstanceOf(NotificadorDeAlerta);
+  });
 
-    it("Verifica que la instancia sea de NotificadorDeAlerta", () => {
-        expect(instance).toBeInstanceOf(NotificadorDeAlerta);
-    });
-
-    it("Verifica que la notificación se realice de forma correcta", () => {
-        expect(instance.notificarAlerta(alertaEstandar)).toBe(`Mensaje: ${alertaEstandar.obtenerMensajeDeAlerta()} 
+  it("Verifica que la notificación se realice de forma correcta", () => {
+    expect(instance.notificarAlerta(alertaEstandar))
+      .toBe(`Mensaje: ${alertaEstandar.obtenerMensajeDeAlerta()} 
         \nTipo de alerta: ${alertaEstandar.obtenerTipoDeAlerta()} 
         \nFecha: ${alertaEstandar.obtenerTimestampDeAlerta()}`);
-    });
-
-})
+  });
+});
