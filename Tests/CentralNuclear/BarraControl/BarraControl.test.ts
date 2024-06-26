96e974161d7416e46666bc92f0d96eb9dfdfbef5
matiGGV <matiggv@protonmail.com>
2024-06-26 19:33:10 -0300

    Tests agregados para States Barra Control

--- Tests/CentralNuclear/BarraControl/BarraControl.test.ts
+++ Tests/CentralNuclear/BarraControl/BarraControl.test.ts
@@ -1,11 +1,15 @@
 import BarraControl from "../../../CentralNuclear/BarrasDeControl/BarraControl";
-import { EstadoBarraDeControl } from "../../../CentralNuclear/BarrasDeControl/EstadoBarraDeControl";
+import EstadoBarraControl from "../../../CentralNuclear/BarrasDeControl/EstadosBarraControl/EstadoBarraControl";
+import EnDesuso from "../../../CentralNuclear/BarrasDeControl/EstadosBarraControl/EnDesuso";
+import Insertada from "../../../CentralNuclear/BarrasDeControl/EstadosBarraControl/Insertada";
+import Eliminada from "../../../CentralNuclear/BarrasDeControl/EstadosBarraControl/Eliminada";
 
 describe("Test de Barra de Control", () => {
   let instance: BarraControl;
 
   beforeEach(() => {
-    instance = new BarraControl("Uranio", 200, EstadoBarraDeControl.EN_DESUSO);
+    let defaultState: EstadoBarraControl = new EnDesuso();
+    instance = new BarraControl("Uranio", 200, defaultState);
   });
 
   it("Verifica que no esté activa", () => {
---
+++ Tests/CentralNuclear/BarraControl/Eliminada.test.ts
@@ -0,0 +1,37 @@
+import BarraControl from "../../../CentralNuclear/BarrasDeControl/BarraControl";
+import EstadoBarraControl from "../../../CentralNuclear/BarrasDeControl/EstadosBarraControl/EstadoBarraControl";
+import Insertada from "../../../CentralNuclear/BarrasDeControl/EstadosBarraControl/Insertada";
+
+describe("Test de Estado Barra de Control: Insertada", () => {
+  let rodInstance: BarraControl;
+  let stateInstance: EstadoBarraControl;
+
+  beforeEach(() => {
+    stateInstance = new Insertada();
+    rodInstance = new BarraControl("Uranio", 200, stateInstance);
+  });
+
+  it("Verifica que esté activo", () => {
+    let estado: boolean = stateInstance.estaActivo();
+    expect(estado).toBe(true);
+  });
+
+  it("Verifica que siga activa", () => {
+    rodInstance.activar();
+    let estado: boolean = rodInstance.estaActivo();
+    expect(estado).toBe(true);
+  });
+
+  it("Verifica que pueda activarse y desactivarse", () => {
+    let estado: boolean = rodInstance.estaActivo();
+    expect(estado).toBe(true);
+
+    rodInstance.desactivar();
+    estado = rodInstance.estaActivo();
+    expect(estado).toBe(false);
+
+    rodInstance.activar();
+    estado = rodInstance.estaActivo();
+    expect(estado).toBe(true);
+  });
+});
