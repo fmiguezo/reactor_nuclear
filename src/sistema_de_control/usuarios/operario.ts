2a0271d50f1923929530889f8d5c4f248100923e
fmiguezo <fmiguezol@proton.me>
2024-06-29 14:18:17 -0300

    Correccion de nombre de clase implementada en Operario

--- src/sistema_de_control/usuarios/operario.ts
+++ src/sistema_de_control/usuarios/operario.ts
@@ -1,7 +1,7 @@
-import ISuscriptor from "../interfaces/empleado";
+import IEmpleado from "../interfaces/empleado";
 import Alerta from "../alertas/alerta";
 
-export default abstract class Operador implements ISuscriptor {
+export default abstract class Operador implements IEmpleado {
   notificar(a: Alerta): void {
     // TO-DO
   }
