162761bbbeac8281c10253c1f10aa615f39728b0
matiGGV <matiggv@protonmail.com>
2024-06-29 19:25:29 -0300

    Builder implementado para reactor + various fixes

---
+++ src/central_nuclear/reactor/builder/director_build_reactor.ts
@@ -0,0 +1,28 @@
+import PlantaNuclear from "../../../planta_nuclear";
+import Reactor from "../reactor";
+import IBuilder from "./ibuilder";
+export default class DirectorBuildReactor {
+  private _builder: IBuilder;
+  private _plantaNuclear!: PlantaNuclear;
+
+  constructor(builder: IBuilder) {
+    this._builder = builder;
+  }
+
+  public cargarPlantaNuclear(plantaNuclear: PlantaNuclear): void {
+    this._plantaNuclear = plantaNuclear;
+  }
+
+  public buildReactorNormal(): Reactor {
+    this._builder.reset();
+    this._builder.setAdminBarras();
+    this._builder.setBarras();
+    this._builder.setSensores();
+    this._builder.setPlantaNuclear(this._plantaNuclear);
+    return this._builder.getReactor();
+  }
+
+  public cambiarBuilder(builder: IBuilder): void {
+    this._builder = builder;
+  }
+}
