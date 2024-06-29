8a1d12c7aff24b4516a9dd14e9b5de2fb6183197
Santinoco <santinot002@gmail.com>
2024-06-29 15:56:42 -0300

    Se Simplifican los comandos, obtener energia y obtener temperatura en un solo comando que nos brinda la informacion del momento de todos los sensores que contiene el reactor, Ahora es mas simple y entendible

--- SistemaDeControl/Command/ObtenerTemperatura.ts
+++
@@ -1,8 +0,0 @@
-import Command from "./Command";
-import Reactor from "../../src/central_nuclear/reactor/reactor";
-
-export default class ObtenerTemperatura implements Command {
-    public ejecutar(r: Reactor): void {
-        console.log(r.getTemperatura())
-    }
-}
