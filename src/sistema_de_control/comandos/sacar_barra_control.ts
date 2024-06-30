c24df7dbaabeb35f6ba30dbef1d93e7b41de21ed
Santinoco <santinot002@gmail.com>
2024-06-30 02:12:12 -0300

    Agrego comando para remover todas las barras de control del reactor en caso de que se solicite

--- src/sistema_de_control/comandos/sacar_barra_control.ts
+++ src/sistema_de_control/comandos/sacar_barra_control.ts
@@ -1,8 +1,12 @@
 import Reactor from "../../central_nuclear/reactor/reactor";
 import Command from "./command";
+import AdministradorBarras from "../../central_nuclear/reactor/administrador/administrador_barras";
+import BarraControl from "../../central_nuclear/barras_control/barra_control";
 
-export default class InsertarBarraDeControl implements Command {
+export default class SacarBarrasDeControl implements Command {
   public ejecutar(r: Reactor): void {
+    let barrasDeControl : BarraControl[] = r.getBarrasDeControl();
+    r.getAdministradorBarras().removerBarras(barrasDeControl);
     throw new Error("Metodo no implementado aun");
   }
 }
