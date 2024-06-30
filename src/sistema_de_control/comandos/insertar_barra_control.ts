252d16e29e6c501189f2157cf12b74050b67d4ba
Santinoco <santinot002@gmail.com>
2024-06-30 02:42:50 -0300

    Modificacion a instertar_barra_control ( A revisar, tratar de ver si se puede implementar la logica en la barra de control

--- src/sistema_de_control/comandos/insertar_barra_control.ts
+++ src/sistema_de_control/comandos/insertar_barra_control.ts
@@ -3,6 +3,11 @@ import Reactor from "../../central_nuclear/reactor/reactor";
 
 export default class InsertarBarraDeControl implements Command {
   public ejecutar(r: Reactor): void {
-    r.getAdministradorBarras().insertarBarras();
+    
+    while(r.getTemperatura() > 330){
+      r.getAdministradorBarras().insertarBarras(1);
+    }
+
+
   }
 }
