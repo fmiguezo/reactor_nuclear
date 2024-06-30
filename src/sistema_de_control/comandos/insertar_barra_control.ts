1c6dddd963c421020cf308b0dd56b5827c116f09
Santinoco <santinot002@gmail.com>
2024-06-30 13:00:15 -0300

    mod

--- src/sistema_de_control/comandos/insertar_barra_control.ts
+++ src/sistema_de_control/comandos/insertar_barra_control.ts
@@ -4,9 +4,11 @@ import Reactor from "../../central_nuclear/reactor/reactor";
 export default class InsertarBarraDeControl implements Command {
   public ejecutar(r: Reactor): void {
     
-    while(r.getTemperatura() > 330){
+  // Es momentaneo, hay que implementar el metodo insertar barra en cada estado para no tener que verificar en esta clase
+   
+  while(r.getTemperatura() > 330){
       r.getAdministradorBarras().insertarBarras(1);
-    }
+  }
 
 
   }
