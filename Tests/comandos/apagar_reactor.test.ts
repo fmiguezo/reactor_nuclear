b25a59e838bbcefe909569b80d0bc64f2b53ad1c
Santinoco <santinot002@gmail.com>
2024-06-30 12:05:34 -0300

    Se corrige test apagar reactor

--- Tests/comandos/apagar_reactor.test.ts
+++ Tests/comandos/apagar_reactor.test.ts
@@ -1,5 +1,8 @@
-import Command from "../../src/sistema_de_control/comandos/command"
-import ApagarReactor from "../../src/sistema_de_control/comandos/apagar_reactor"
+import Command from "../../src/sistema_de_control/comandos/command";
+import ApagarReactor from "../../src/sistema_de_control/comandos/apagar_reactor";
+import Reactor from "../../src/central_nuclear/reactor/reactor";
+import RNormal from "../../src/central_nuclear/reactor/estados_reactor/normal";
+
 
 describe("Test del comando Apagar reactor", () => {
     let reactor: Reactor;
@@ -8,7 +11,14 @@ describe("Test del comando Apagar reactor", () => {
     beforeEach(() => {
       reactor = new Reactor();
       apagarReactor = new ApagarReactor();
+      MockEncendido = new RNormal(reactor) as jest.Mocked<RNormal>;
+      reactor.setEstado(MockEncendido);
     });
+    
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+    
   
     test("debería apagar el reactor", () => {
       expect(reactor.getEstado().estaEncendido()).toBe(true);
