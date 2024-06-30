6d770276fa0358f504fb530ea4a66e1870d94f8d
M12 <matias.serro@gmail.com>
2024-06-30 12:21:37 -0300

    Problema con los test setSensores y setPlantaNuclear. No tenemos un getPlantaNuclear dentro del Reactor

--- Tests/central_nuclear/reactor/builder/builder_reactor_normal.test.ts
+++ Tests/central_nuclear/reactor/builder/builder_reactor_normal.test.ts
@@ -1,5 +1,8 @@
+import AdministradorBarras from "../../../../src/central_nuclear/reactor/administrador/administrador_barras";
 import BuilderReactorNormal from "../../../../src/central_nuclear/reactor/builder/builder_reactor_normal";
 import Reactor from "../../../../src/central_nuclear/reactor/reactor";
+import ISensor from "../../../../src/central_nuclear/interfaces/isensor";
+import PlantaNuclear from "../../../../src/planta_nuclear";
 
 describe("Test de BuilderReactorNormal", () => {
   let instance: BuilderReactorNormal;
@@ -13,5 +16,29 @@ describe("Test de BuilderReactorNormal", () => {
     expect(() => instance.getReactor).toBeInstanceOf(Reactor);
   })
 
+  it("Verifica que el administrador se agregue de forma correcta", () => {
+    instance.reset();
+    instance.setAdminBarras();
+    expect(() => instance.getReactor().getAdministradorBarras()).toBeInstanceOf(AdministradorBarras);
+  })
+
+  it("Verifica que las barras se agreguen de forma correcta", () => {
+    instance.reset();
+    instance.setBarras();
+    expect(() => instance.getReactor().getAdministradorBarras().getBarrasEnDesuso()).toBe(100);
+  })
+
+  it("Verifica que los sensores se agreguen de forma correcta", () => {
+    instance.reset();
+    instance.setSensores();
+    expect(() => instance.getReactor().getSensores()).toBeInstanceOf(ISensor);
+  })
+
+  it("Verifica que la planta nuclear se agregue de forma correcta", () => {
+    instance.reset();
+    let planta = new PlantaNuclear();
+    instance.setPlantaNuclear(planta);
+    expect(() => instance.getReactor().getPlantaNuclear()).toBeInstanceOf(PlantaNuclear);
+  })
 
 });
