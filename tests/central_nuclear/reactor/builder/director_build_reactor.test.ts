0da882dec07983b87c2cdf7cb181de6791f1d728
Florencia Miguez <46388949+fmiguezo@users.noreply.github.com>
2024-07-01 17:15:14 -0300

    cambio en el test director builder reactor

--- tests/central_nuclear/reactor/builder/director_build_reactor.test.ts
+++ tests/central_nuclear/reactor/builder/director_build_reactor.test.ts
@@ -4,28 +4,30 @@ import Reactor from "../../../../src/central_nuclear/reactor/reactor";
 import PlantaNuclear from "../../../../src/planta_nuclear";
 
 describe("Test de DirectorBuilderReactor", () => {
-  let instance = new DirectorBuilderReactor(new BuilderReactorNormal());
+  let MockBuilderConcreto: jest.Mocked<BuilderReactorNormal> =
+    new BuilderReactorNormal() as jest.Mocked<BuilderReactorNormal>;
+  let instance = new DirectorBuilderReactor(MockBuilderConcreto);
 
   beforeEach(() => {});
 
   it("Verifica que la instancia sea de tipo DirectorBuilderReactor", () => {
-    expect(() => instance).toBeInstanceOf(DirectorBuilderReactor);
+    expect(instance).toBeInstanceOf(DirectorBuilderReactor);
   });
 
   it("Verifica que la planta nuclear se cargue correctamente", () => {
     let planta = new PlantaNuclear();
     instance.cargarPlantaNuclear(planta);
-    expect(() => instance.getPlantaNuclear()).toBeInstanceOf(PlantaNuclear);
+    expect(instance.getPlantaNuclear()).toBeInstanceOf(PlantaNuclear);
   });
 
   it("Verifica que el reactor se construya correctamente", () => {
     instance.buildReactorNormal();
-    expect(() => instance.getBuilder().getReactor()).toBeInstanceOf(Reactor);
+    expect(instance.getBuilder().getReactor()).toBeInstanceOf(Reactor);
   });
 
   it("Verifica que el cambiar builder funcione", () => {
     let builder = new BuilderReactorNormal();
     instance.cambiarBuilder(builder);
-    expect(() => instance.getBuilder()).toBeInstanceOf(BuilderReactorNormal);
+    expect(instance.getBuilder()).toBeInstanceOf(BuilderReactorNormal);
   });
 });
