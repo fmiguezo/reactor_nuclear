a2ecb0da84c0ab2e4e55f87b072d998d7b5df1fd
juan <jpverdondoni@gmail.com>
2024-07-02 12:50:32 -0300

    Saque este test de: no debería calcular energía termal ni neta si el reactor está apagado, dado que no tiene sentido por que no esta abstraida el calculo de energia neta ni de energiaTermal en cada estado

--- tests/central_nuclear/reactor/estados_reactor/apagado.test.ts
+++ tests/central_nuclear/reactor/estados_reactor/apagado.test.ts
@@ -36,20 +36,13 @@ describe("Test del estado apagado", () => {
     expect(MockReactor.getEstado()).toBeInstanceOf(RApagado);
   });
 
-  it("no debería calcular energía termal ni neta si el reactor está apagado", () => {
-    expect(() => MockReactor.obtenerEnergiaTermal()).toThrow(
-      "No se alcanza la temperatura minima para generar energia"
-    );
-    expect(() => MockReactor.obtenerEnergiaNeta()).toThrow("No se alcanza la temperatura minima para generar energia");
-  });
-
   it("debería mantener el estado apagado si la temperatura del reactor es igual a 0", () => {
     MockReactor.setTemperatura(0);
     instance.verificarEstado();
     expect(MockReactor.estaEncendido()).toBe(false);
   });
 
-  it.only("debería cambiar el estado a encendiendo si se llama al metodo encender", () => {
+  it("debería cambiar el estado a encendiendo si se llama al metodo encender", () => {
     instance.encender();
     expect(MockReactor.getEstado()).toBeInstanceOf(REncenciendo);
     expect(MockReactor.estaEncendido()).toBeTruthy;
