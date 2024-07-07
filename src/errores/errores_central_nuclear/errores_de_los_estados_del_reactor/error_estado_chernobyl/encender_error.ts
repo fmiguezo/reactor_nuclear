db3662549276f23aae51bbac7e233ac0979dc7c7
matiGGV <matiggv@protonmail.com>
2024-07-07 19:17:52 -0300

    Rama mergeada con JuanTest

--- src/errores/errores_central_nuclear/errores_de_los_estados_del_reactor/error_estado_chernobyl/encender_error.ts
+++ src/errores/errores_central_nuclear/errores_de_los_estados_del_reactor/error_estado_chernobyl/encender_error.ts
@@ -7,10 +7,4 @@ export default class EncenderError extends Error {
     super(message);
     this.name = "EncenderError";
   }
-  constructor(
-    message: string = Constantes.MENSAJE_ESTADO_CHERNOBYL_NO_ENCENDIO
-  ) {
-    super(message);
-    this.name = "EncenderError";
-  }
 }
