db7bf232c5e6ffe1fcdcb909207e87f4d69f4c4e
matiGGV <matiggv@protonmail.com>
2024-07-11 01:35:14 -0300

    Cambio en test reactor y test eliminada al 100

--- tests/central_nuclear/barras_control/estados/eliminada.test.ts
+++ tests/central_nuclear/barras_control/estados/eliminada.test.ts
@@ -3,18 +3,49 @@ import ActivarError from "../../../../src/errores/errores_central_nuclear/errore
 import DesactivarError from "../../../../src/errores/errores_central_nuclear/errores_barras_de_control/error_estado_eliminada/desactivar_error";
 import RegistroBarrasUsadas from "../../../../src/sistema_de_control/registros/registro_barras_usadas";
 import BarraControlCadmio from "../../../../src/central_nuclear/barras_control/barra_control_cadmio";
+import EstadoBarraControl from "../../../../src/central_nuclear/barras_control/estados/estado_barra_control";
 
 describe("Eliminada", () => {
   let eliminada: Eliminada;
   let rodInstance: BarraControlCadmio;
+
   beforeEach(() => {
     eliminada = new Eliminada();
-    rodInstance = new BarraControlCadmio(200,eliminada);
+    rodInstance = new BarraControlCadmio(200, eliminada);
     eliminada.setBarraControl(rodInstance);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
+  });
+
+  it("Constructor debería llamar al constructor de la clase padre", () => {
+    // Elimina mocks anteriores
+    jest.resetModules();
+
+    // Crea el Mock de EstadoBarraControl para este test
+    const EstadoBarraControl =
+      require("../../../../src/central_nuclear/barras_control/estados/estado_barra_control").default;
+    jest.mock(
+      "../../../../src/central_nuclear/barras_control/estados/estado_barra_control"
+    );
+
+    // Crea el spy para el constructor de EstadoBarraControl
+    const superSpy = jest.spyOn(EstadoBarraControl.prototype, "constructor");
+
+    const Eliminada =
+      require("../../../../src/central_nuclear/barras_control/estados/eliminada").default;
+    new Eliminada();
+
+    expect(superSpy).toHaveBeenCalled();
+  });
+
   it("Constructor debería llamar a reportarVencimiento", () => {
-    const insertarRegistroSpy = jest.spyOn(RegistroBarrasUsadas.instancia, "insertarRegistro");
+    const insertarRegistroSpy = jest.spyOn(
+      RegistroBarrasUsadas.instancia,
+      "insertarRegistro"
+    );
     new Eliminada(); // Llamamos al constructor para verificar su comportamiento
     expect(insertarRegistroSpy).toHaveBeenCalledWith(1);
   });
@@ -25,12 +56,16 @@ describe("Eliminada", () => {
 
   it("activar debería tirar ActivarError con el mensaje de barra vencida", () => {
     expect(() => eliminada.activar()).toThrow(ActivarError);
-    expect(() => eliminada.activar()).toThrow("La barra está vencida. No puede utilizarse.");
+    expect(() => eliminada.activar()).toThrow(
+      "La barra está vencida. No puede utilizarse."
+    );
   });
 
   it("desactivar debería tirar DesactivarError con el mensaje de barra vencida", () => {
     expect(() => eliminada.desactivar()).toThrow(DesactivarError);
-    expect(() => eliminada.desactivar()).toThrow("La barra está vencida. No puede utilizarse.");
+    expect(() => eliminada.desactivar()).toThrow(
+      "La barra está vencida. No puede utilizarse."
+    );
   });
 
   it("calcPctBarra debería devolver 0", () => {
@@ -38,21 +73,23 @@ describe("Eliminada", () => {
   });
 
   it("reportarVencimiento debería insertar el registo en RegistroBarrasUsadas", () => {
-    const insertarRegistroSpy = jest.spyOn(RegistroBarrasUsadas.instancia, "insertarRegistro");
+    const insertarRegistroSpy = jest.spyOn(
+      RegistroBarrasUsadas.instancia,
+      "insertarRegistro"
+    );
     // Accedemos al método privado usando la indexación de propiedades
     // @ts-ignore
     eliminada["reportarVencimiento"]();
     expect(insertarRegistroSpy).toHaveBeenCalledWith(1);
   });
 
-  it('Verifica que el getter de la barra funcione correctamente', () => {
+  it("Verifica que el getter de la barra funcione correctamente", () => {
     expect(eliminada.getBarraControl()).toBe(rodInstance);
   });
 
-  it('Verifica que el setter de la barra funcione correctamente', () => {
+  it("Verifica que el setter de la barra funcione correctamente", () => {
     let rodInstance1 = new BarraControlCadmio(200, eliminada);
     eliminada.setBarraControl(rodInstance1);
     expect(eliminada.getBarraControl()).toBe(rodInstance1);
   });
-
 });
