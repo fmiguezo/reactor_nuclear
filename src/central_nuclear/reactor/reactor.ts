b2c80e1def1d1bdba41878353d9a5e6ca59267db
juan <jpverdondoni@gmail.com>
2024-07-11 12:19:12 -0300

    Cambie un valor divisorio

--- src/central_nuclear/reactor/reactor.ts
+++ src/central_nuclear/reactor/reactor.ts
@@ -173,7 +173,7 @@ export default class Reactor {
       administradorBarras.getBarrasInsertadas();
     let valorEnfriamiento: number = 0;
     barrasInsertadas.forEach((b) => (valorEnfriamiento += b.getPctBarra()));
-    return valorEnfriamiento;
+    return (valorEnfriamiento/420);
   }
 
   public incrementarTemperatura(): void {
